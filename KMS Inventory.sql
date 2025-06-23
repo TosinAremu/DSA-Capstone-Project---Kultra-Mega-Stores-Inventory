26ad9e3fbca018b400267e6beebdf80edbf7c8a4
Tosin Aremu <tosin.aremuofficial@gmail.com>
2025-06-23 14:31:34 +0100

    Update KMS Inventory.sql

--- KMS Inventory.sql
+++ KMS Inventory.sql
@@ -81,7 +81,7 @@ GROUP BY
 ORDER BY 
     Total_Sales DESC
 
-8. ----------Which Corporate Customer placed the most number of orders in 2009 – 2012?
+8. ----------Which Corporate Customer placed the most number of orders in 2009 â€“ 2012?
 SELECT TOP 1 Customer_Name,
     COUNT(DISTINCT [Order_ID]) AS Orders_Count
 FROM 
@@ -106,10 +106,142 @@ GROUP BY
 ORDER BY 
     Total_Profit DESC
 
+------Joining Order Status with KMS Inventory Table
+SELECT * FROM [dbo].Order_Status
+SELECT * FROM [dbo].[KMS Inventory]
+
+
+SELECT
+	[KMS Inventory].Order_ID,
+	[KMS Inventory].Customer_Name,
+	[KMS Inventory].Order_Date,
+	[KMS Inventory].Product_Category,
+	[KMS Inventory].Sales,
+	Order_Status.Status
+FROM
+	[KMS Inventory]
+LEFT JOIN
+	Order_Status
+ON
+	[KMS Inventory].Order_ID = Order_Status.Order_ID
+
+
 10 ---- Which customer returned items, and what segment do they belong to?
-SELECT Customer_Name,
-    Customer_Segment
+SELECT 
+	[KMS Inventory].Customer_Name,
+	[KMS Inventory].Customer_Segment,
+	[KMS Inventory].Order_ID
 FROM [KMS Inventory]
-WHERE 
-    Profit < 0;
+INNER JOIN 
+	(
+	VALUES  (65, 'Returned'), 
+			(69, 'Returned'), 
+			(134, 'Returned'), 
+			(230, 'Returned'),
+			(324, 'Returned'),
+			(359, 'Returned'),
+			(612, 'Returned'),
+			(614, 'Returned'),
+			(678, 'Returned'),
+			(710, 'Returned'),
+			(740, 'Returned'),
+			(775, 'Returned'),
+			(833, 'Returned'),
+			(902, 'Returned'),
+			(928, 'Returned'),
+			(930, 'Returned'),
+			(1060, 'Returned'),
+			(1127, 'Returned'),
+			(1285, 'Returned'),
+			(1317, 'Returned'),
+			(1382, 'Returned'),
+			(1538, 'Returned'),
+			(1665, 'Returned'),
+			(1921, 'Returned'),
+			(2050, 'Returned'),
+			(2052, 'Returned'),
+			(2055, 'Returned'),
+			(2146, 'Returned'),
+			(2213, 'Returned'),
+			(2240, 'Returned'),
+			(2500, 'Returned'),
+			(2503, 'Returned'),
+			(2562, 'Returned'),
+			(2823, 'Returned'),
+			(2915, 'Returned'),
+			(3300, 'Returned'),
+			(3456, 'Returned'),
+			(3525, 'Returned'),
+			(3589, 'Returned'),
+			(3687, 'Returned'),
+			(3777, 'Returned'),
+			(3783, 'Returned'),
+			(4006, 'Returned'),
+			(4037, 'Returned'),
+			(4230, 'Returned'),
+			(4261, 'Returned'),
+			(4391, 'Returned'),
+			(4610, 'Returned'),
+			(4738, 'Returned'),
+			(4864, 'Returned'),
+			(4960, 'Returned'),
+			(5028, 'Returned'),
+			(5059, 'Returned'),
+			(5061, 'Returned'),
+			(5189, 'Returned'),
+			(5381, 'Returned'),
+			(5414, 'Returned'),
+			(5511, 'Returned'),
+			(5699, 'Returned'),
+			(6054, 'Returned'),
+			(6241, 'Returned'),
+			(6272, 'Returned'),
+			(6498, 'Returned'),
+			(6500, 'Returned'),
+			(6502, 'Returned'),
+			(6661, 'Returned'),
+			(6695, 'Returned'),
+			(6757, 'Returned'),
+			(6978, 'Returned'),
+			(6979, 'Returned'),
+			(7079, 'Returned'),
+			(7107, 'Returned'),
+			(7203, 'Returned'),
+			(7269, 'Returned'),
+			(7364, 'Returned'),
+			(7521, 'Returned'),
+			(7744, 'Returned'),
+			(7812, 'Returned'),
+			(7815, 'Returned'),
+			(7841, 'Returned'),
+			(7845, 'Returned'),
+			(8034, 'Returned'),
+			(8133, 'Returned'),
+			(8292, 'Returned'),
+			(8293, 'Returned'),
+			(8353, 'Returned'),
+			(8961, 'Returned'),
+			(9027, 'Returned'),
+			(9093, 'Returned'),
+			(9152, 'Returned'),
+			(9219, 'Returned'),
+			(9472,	'Returned'),
+			(9574,	'Returned'),
+			(9696,	'Returned'),
+			(9701,	'Returned'),
+			(9762,	'Returned'),
+			(9829,	'Returned'),
+			(9895,	'Returned'),
+			(9923,	'Returned'),
+			(9927,	'Returned'),
+			(10054,	'Returned'),
+			(10183,	'Returned'),
+			(10498,	'Returned')
+
+	)
+	AS Order_Status([Order_ID], Status)
+ON
+	[KMS Inventory].Order_ID = Order_Status.Order_ID;
+     
+
 
